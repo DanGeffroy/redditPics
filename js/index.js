9ecf431b61e79097d7be0440e8fa0c8e961dbfb4
Dan Geffroy <dan.geffroy@etu.univ-nantes.fr>
2015-11-05 14:27:10 +0100

    adding mobile version + bug correction, now it's beautifull !

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#button").click(function() {
+     scrollToAnchor('anchor');
+  });
+});
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+};
