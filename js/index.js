3d771272e6a9152d73ad38f1c6f3d817d9d5ba58
Dan Geffroy <dan.geffroy@etu.univ-nantes.fr>
2015-11-09 11:42:21 +0100

    Way better pics viewing, next time, clean the code :S

--- js/index.js
+++ js/index.js
@@ -49,48 +49,15 @@ function searchAndPrint(filter,resolutions, subreddit){
 }
 function printPics(data,resolutions){
   option = resolutions;
-  cssAdaptator(resolutions);
+  cssAdaptator();
   for(i = 0;i<100;i++){
   /*  console.log("<div class='image' id='img1'><img src='"+data.data.children[i].data.preview.images[0].resolutions[1]+"' alt='"+data.data.children[i].data.title+"'/><span>"+data.data.children[i].data.title+"</span></div>");*/
   if((typeof data.data.children[i].data.preview != "undefined")&&(typeof data.data.children[i].data.preview.images[0].resolutions[option] != "undefined")){
-    $(".images").append("<div class='image' id='img" + i+"'><div class='imgcropper'><a href='"+data.data.children[i].data.preview.images[0].resolutions[option].url+"'><img src='"+data.data.children[i].data.preview.images[0].resolutions[option].url+"' alt='"+data.data.children[i].data.title+"'/></a></div><span>"+data.data.children[i].data.title+"</span></div>");
+    $(".images").append("<div class='tourImg'><div class='image imgSizeLon"+option+" imgSizeLar"+option+"' id='img" + i+"'><a href='"+data.data.children[i].data.preview.images[0].resolutions[option].url+"'><img class='imgSizeLon"+option+ "' src='"+data.data.children[i].data.preview.images[0].resolutions[option].url+"' alt='"+data.data.children[i].data.title+"'/></a></div><span>"+data.data.children[i].data.title+"</span></div>");
   }
   }
 };
 
-function cssAdaptator(resolutions){
+function cssAdaptator(){
   $(".images").css("visibility", "visible");
-  value ="5";
-  if(resolutions==="0"){
-    value = 9;
-  }
-
-  else if(resolutions==="1"){
-    value = 6;
-  }
-  else if(resolutions==="2"){
-    value = 5;
-  }
-  else if(resolutions==="3"){
-    value = 4;
-  }
-  else if(resolutions==="4"){
-    value = 3;
-  }
-  else if(resolutions==="5"){
-    value = 2;
-  }
-  else if(resolutions==="6"){
-    value = 1;
-  }
-    $(".images").css("-moz-column-count:", value);
-    $(".images").css("-moz-column-gap", value +"%");
-    $(".images").css("-moz-column-width", value +"0%");
-    $(".images").css("-webkit-column-count", value);
-    $(".images").css("-webkit-column-gap", value +"%");
-    $(".images").css("-webkit-column-width", value +"0%");
-    $(".images").css("column-count", value);
-    $(".images").css("column-gap", value +"%");
-    $(".images").css("column-width", value +"0%");
-
 }
